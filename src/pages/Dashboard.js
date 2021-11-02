2b4c2a35635e9e8e115dac0887dab1be7fbd9783
Diana Barney <diana.aiken@gmail.com>
2021-11-02 14:06:48 -0400

    refactored dashboard to fix issue with persisting data for the recent transaction table

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -2,36 +2,54 @@
 //              IMPORTS
 // =======================================
 import { Helmet } from 'react-helmet'
-import { StyledMain, StyledDashboard, StyledTable, StyledForm } from '../styles'
 // ---------- Components ----------
-import TransactionTable from '../components/TransactionsTable'
 import NewTransactionForm from '../components/NewTransactionFom/NewTransactionForm'
+// ---------- Styles ----------
+import { StyledTable } from '../styles'
 
 
-// ========== DEFINE ==========
+// =======================================
+//       DEFINE the COMPONENT
+// =======================================
 const Dashboard = (props) => {
-    console.log(props)
     
-    // ----- RETURN some JSX -----
+    // =======================================
+    //          RETURN some JSX
+    // =======================================
     return (
         <>
             <Helmet>
                 <title>Gifted | {props.user.displayName}'s Dashboard</title>
-                <meta name='description' content='A centralized place to log and see your most recent gifts' />
+                <meta name='description' content='A user friendly gift management system.' />
                 <meta name='keywords' content='React, Gifting, Organization' />
             </Helmet>
-            <StyledMain>
-                <StyledDashboard>
-                    <div className="dashboardBody">
-                        <h1><span>Gifting </span>Dashboard</h1>
-                        <NewTransactionForm createTransaction={props.createTransaction}/>
-                        <TransactionTable transactions={props.transactions} />
-                    </div>
-                </StyledDashboard>
-            </StyledMain>
+            <div>
+                <h1><span>Gifting </span>Dashboard</h1>
+                <NewTransactionForm createTransaction={props.createTransaction} />
+                <StyledTable>
+                    <thead>
+                        <th>Event</th>
+                        <th>Recipient</th>
+                        <th>Gift</th>
+                    </thead>
+                    <tbody>
+                        {
+                            props.transactions.map(t => (
+                                <tr key={t._id}>
+                                    <td>{t.eventId}</td>
+                                    <td>{t.recipientId}</td>
+                                    <td>{t.giftId}</td>
+                                </tr>
+                            ))
+                        }
+                    </tbody>
+                </StyledTable>
+            </div>
         </>
     )
 }
 
-// ========== EXPORT ==========
+// =======================================
+//       EXPORT the COMPONENT
+// =======================================
 export default Dashboard
